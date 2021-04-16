7208fe586079e5b5edf16bce5cf4576acacfd659
Huang-Tsu <huang0966714@gmail.com>
2021-04-16 17:49:28 +0800

    Correcting partition function.
    The initial value of i and j in partition should be:
    i:head-1
    j:head
    This change bring the effiency of the program to another level.
    
    I set them as -1 and 0, and that's the reason why the program run so slow.

--- quick_sort.c
+++ quick_sort.c
@@ -10,7 +10,7 @@ int main(){
 	array = (int*)calloc((int)1e7, sizeof(int));
 	int cnt = 0;
 
-	while(scanf("%d", &array[cnt++]) != EOF);
+	while(scanf("%d", &array[cnt]) != EOF) cnt++;
 
 	quick_sort(array, 0, cnt-1);
 
@@ -24,8 +24,7 @@ int main(){
 void quick_sort(int *array, int head, int tail){
 	if(head >= tail) return;
 
-	int pivot;
-	pivot = partition(array, head, tail);
+	int pivot = partition(array, head, tail);
 	quick_sort(array, head, pivot-1);	
 	quick_sort(array, pivot+1, tail);
 
@@ -36,7 +35,7 @@ int partition(int *array, int head, int tail){
 	int pivot = array[tail];
 	int temp;
 	int i, j;
-	for(i=-1, j=0; j<tail; j++){
+	for(i=head-1, j=head; j<tail; j++){
 		if(array[j] <= pivot){
 			i++;
 			temp = array[j];
@@ -48,5 +47,5 @@ int partition(int *array, int head, int tail){
 	temp = array[j];
 	array[j] = array[i];
 	array[i] = temp;
-
+	return i;
 }
