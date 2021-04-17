64127048fefc8ae30aa4212bd856a1e756dd4053
Huang-Tsu <huang0966714@gmail.com>
2021-04-17 16:03:18 +0800

    Change in quick sort:
    Adding timeval to record sorting time.
    Adding a print result function.
    use "resgiter int *" to declare that "array" will be used heavely.

--- quick_sort.c
+++ quick_sort.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
+typedef struct timeval Timeval;
 
 void quick_sort(int *array, int head, int tail);
 int partition(int *array, int head, int tail);
+void print_result(int *array, int cnt);
 
 int main(){
 	int input;
-	int *array;
+	register int *array;
 	array = (int*)calloc((int)1e7, sizeof(int));
 	int cnt = 0;
+	Timeval start;
+	Timeval end;
+	unsigned long diff;
 
 	while(scanf("%d", &array[cnt]) != EOF) cnt++;
 
+	gettimeofday(&start, NULL);
 	quick_sort(array, 0, cnt-1);
+	gettimeofday(&end, NULL);
+	diff = 1000000*(end.tv_sec-start.tv_sec) + end.tv_usec-start.tv_usec;
 
-	for(int i=0; i<cnt; i++){
-		printf("%d\n", array[i]);
-	}
+	//printf("sorting time:%f\n", diff/1000000.0);
+	print_result(array, cnt);
 
 	free(array);
 	return 0;
@@ -49,4 +58,9 @@ int partition(int *array, int head, int tail){
 	array[i] = temp;
 
 	return i;
+}
+void print_result(int *array, int cnt){
+	for(int i=0; i<cnt; i++){
+		printf("%d\n", array[i]);
+	}
 }
