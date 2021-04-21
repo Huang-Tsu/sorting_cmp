89afef63fe65c722c3d466f849ee04f11caa7093
Huang-Tsu <huang0966714@gmail.com>
2021-04-21 15:38:05 +0800

    Completion of heap sort.
    Finally finished the heap sort.
    	The points worth mentioning:
    1.The first element is stored in idx 1.
    	If idx=i, then the left node is i*2, right node is i*2+1
    2.heapify is the key of heap sort.
    	The method to implement it is:
    		compare the node:
    			The termination condition is idx*2>len, which means idx doesn't have any child node.
    			1.First set the root node are largest.
    			2.Second compared largest with left node.
    			3.Third compared largest with right node.
    			4.Check if the root node is still the largest, if so, the node  beneath it are all heapified, thus return. If not, then swap root node with largest node, and continue heapify with "new root idx", that is, the largest (recusive).
    3.The way to build heap
    	->heapify every node from the bottom(the last node which has child node) to the top(root).

--- heap_sort.c
+++ heap_sort.c
@@ -4,13 +4,12 @@
 
 typedef struct timeval Timeval;
 
-void heapify(int *array, int idx, int len);
-void build_heap(int *array, int idx, int len);
-void heap_sort(int *array, int idx);
+void heapify(int *array, int root, int len);
+void heap_sort(int *array, int len);
 void print_result(int *array, int cnt);
+void swap(int *a, int *b);
 
 int main(){
-	int input;
 	int idx=1;
 	register int *array;
 	array = (int*)calloc((int)1e7, sizeof(int));
@@ -21,31 +20,55 @@ int main(){
 	while(scanf("%d", &array[idx]) != EOF) idx++;
 
 	gettimeofday(&start, NULL);
-	build_heap(array, 1, idx-1);
-	heap_sort(array, 1, idx-1);
+	heap_sort(array, idx-1);
 	gettimeofday(&end, NULL);
 	diff = 1000000*(end.tv_sec-start.tv_sec) + end.tv_usec-start.tv_usec;
 
-	//printf("sorting time:%f\n", diff/1000000.0);
-	print_result(array, cnt);
+	printf("sorting time:%f\n", diff/1000000.0);
+	//print_result(array, idx);
 
 	free(array);
 	return 0;
 }
 
-void heap_sort(int *array, int idx){
-	if(heap_len <= 1) return;
+void heap_sort(int *array, int len){
+	for(int i=len/2; i>0; i--){
+		heapify(array, i, len);
+	}
 
-	
+	while(len>1){
+		swap(&array[1], &array[len--]);
+		heapify(array, 1, len);
+	}
 }
-void heapify(int *array, int idx, int len){
-	if(idx>len/2) return;	
+void heapify(int *array, int root, int len){
+	if(root*2 > len) return;	
+
+	int largest = root;
+	int left = root*2;
+	int right = root*2+1;
+
+	if(array[left]>array[largest]){ 
+		largest = left;
+	}
+	if(right<=len && array[right]>array[largest]){
+		largest = right;
+	}
+
+	if(largest != root){
+		swap(&array[root], &array[largest]);
+		heapify(array, largest, len);
+	}
+	else return;
+
 }
 inline void print_result(int *array, int cnt){
-	for(int i=0; i<cnt; i++){
+	for(int i=1; i<cnt; i++){
 		printf("%d\n", array[i]);
 	}
 }
-void build_heap(int *array, int idx, int len){
-	
+void swap(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
