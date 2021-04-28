6f2a184983689af923a4d24f180aa99e80809124
Huang-Tsu <huang0966714@gmail.com>
2021-04-28 20:57:38 +0800

    Side function

---
+++ side_function.c
@@ -0,0 +1,13 @@
+#include "sorting.h"
+#include <stdio.h>
+#include <string.h>
+void read_num_rand(int *array){
+	int i=0;
+	while(scanf("%s", array[i]) !=EOF)
+		;
+}
+void read_eng_rand(char **array){
+	int i=0;
+	while(scanf("%s", array[i]) !=EOF)
+		;
+}
