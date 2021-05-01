39b44cc51e721016e5fa13143a1d03c414934c13
Huang-Tsu <huang0966714@gmail.com>
2021-05-01 16:48:21 +0800

    The process to complete main.c
    可以讀入數字，也可以sorting了

--- main.c
+++ main.c
@@ -7,39 +7,45 @@
 typedef struct timeval Timeval;
 
 int main(){
-	register int *array_num = (int*)calloc((int)1e7, sizeof(int));
-	int *temp_rand_num = (int*)calloc((int)1e7, sizeof(int));
-	register char **array_eng = (char**)calloc((int)1e7, sizeof(char*));
-	char **temp_rand_eng = (char**)calloc((int)1e7, sizeof(char*));
-	int cnt = 0;
+	register int *number_array = (int*)calloc((int)1e7, sizeof(int));
+	register int *temp_random_number = (int*)calloc((int)1e7, sizeof(int));
+	//register char **english_array = (char**)calloc((int)1e7, sizeof(char*));
+	//register char **temp_random_english = (char**)calloc((int)1e7, sizeof(char*));
 	Timeval start;
 	Timeval end;
 	unsigned long diff;
 
+	/*
 	for(int i=0; i<RAND_LEN; i++){
-		array_eng[i] = (char*)calloc(101, sizeof(char));
-		temp_rand_eng = (char**)calloc(101, sizeof(char));
+		english_array[i] = (char*)calloc(101, sizeof(char));
+		temp_random_english[i] = (char*)calloc(101, sizeof(char));
 	}
+	*/
+		printf("Finished allocation.\n");
 
-	read_num_rand(temp_rand_num);	
-	read_eng_rand(temp_rand_eng);
+		//initialize
+	input_random_number(temp_random_number);	
+	//input_random_english(temp_random_english);
 
 
+	copy_number_array(number_array, temp_random_number, RAND_LEN);	//copy_number_array(to, from, len)
+	//copy_english_array(temp_random_english, english_array, RAND_LEN);
 	gettimeofday(&start, NULL);
-	//quick_sort(array_num, 0, cnt-1);
+	quick_sort(number_array, 0, RAND_LEN-1);
 	gettimeofday(&end, NULL);
 	diff = 1000000*(end.tv_sec-start.tv_sec) + end.tv_usec-start.tv_usec;
-
 	printf("sorting time:%f\n", diff/1000000.0);
-	//print_result(array_num, cnt);
+	//print_result(number_array, cnt);
 
-	free(array_num);
-
+	/*
 	for(int i=0; i<RAND_LEN; i++){
-		free(array_eng[i]);
-		free(temp_rand_eng[i]);
+		free(english_array[i]);
+		free(temp_random_english[i]);
 	}
-	free(array_eng);
-	free(temp_rand_eng);
+	*/
+	free(number_array);
+	//free(english_array);
+	free(temp_random_number);
+	//free(temp_random_english);
 	return 0;
 }
