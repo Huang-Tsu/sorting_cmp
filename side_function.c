0bbbb6d4142bf9ac0e1a854aeef37ad31f273013
Huang-Tsu <huang0966714@gmail.com>
2021-05-01 16:50:13 +0800

    Implement some side functions

--- side_function.c
+++ side_function.c
@@ -1,13 +1,32 @@
 #include "sorting.h"
 #include <stdio.h>
 #include <string.h>
-void read_num_rand(int *array){
+void input_random_number(int *array){
+		puts("Hi! I am in input_random_number.");
 	int i=0;
-	while(scanf("%d", &array[i]) !=EOF)
+	while(scanf("%d", &array[i++]) !=EOF)
 		;
+		printf("Finished read number.\n");
 }
-void read_eng_rand(char **array){
+void input_random_english(char **array){
+		puts("Hi! I am in input_random_english.");
 	int i=0;
-	while(scanf("%s", array[i]) !=EOF)
+	while(scanf("%s", array[i++]) !=EOF)
 		;
+		printf("Finished read english.\n");
+}
+void copy_english_array(char **to, char **from, int len){
+	int i;
+	for(i=0; i<len; i++){
+		strcpy(to[i], from[i]);
+	}
+		printf("Finished copy english.\n");
+	
+}
+void copy_number_array(int *to, int *from, int len){
+	int i;
+	for(i=0; i<len; i++){
+		to[i] = from[i];
+	}
+		printf("Finished copy number.\n");
 }
